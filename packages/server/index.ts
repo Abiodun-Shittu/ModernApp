566bc4637d3038c8b26fe30bc6aaa070844b275b
Abiodun Shittu <jerrywizklay1@gmail.com>
2026-03-08 09:15:26 -0400

    connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from the server!!!');
 });
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'This is a JSON response' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
