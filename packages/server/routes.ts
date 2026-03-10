b3e08fdd8ca850b8c7bb3629e0b82947e7c98b62
Abiodun Shittu <jerrywizklay1@gmail.com>
2026-03-10 17:00:46 -0400

    Extracting Routes

---
+++ packages/server/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { chatController } from './controllers/chat.controller';
+
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+   res.send('Hello from the server!!!');
+});
+router.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'This is a JSON response' });
+});
+
+router.post('/api/chat', chatController.sendMessage);
+
+export default router;
