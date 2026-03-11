396e55bd239f19d2add5e70e16de8bcee6d897e2
Abiodun Shittu <jerrywizklay1@gmail.com>
2026-03-11 15:13:46 -0400

    Building the ChaBot Component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,9 @@
-import { Button } from './components/ui/button';
-import { useEffect, useState } from 'react';
+import ChatBot from './components/chatBot';
 
 function App() {
-   const [message, setMessage] = useState('Hello from the client!!!');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((response) => response.json())
-         .then((data) => setMessage(data.message))
-         .catch((error) => console.error('Error fetching message:', error));
-   }, []);
-
    return (
       <div className="p-4">
-         <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me</Button>{' '}
+         <ChatBot />
       </div>
    );
 }
